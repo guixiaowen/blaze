c43a23bac234ffc4e01038a394fcb471342a9149
guihuawen <guihuawen@didiglobal.com>
2025-10-13 12:59:43 +0800

    Follow #1409 Add the conversion execution list in the Spark UI.

--- auron-spark-ui/src/main/scala/org/apache/spark/sql/execution/ui/AuronSQLHistoryServerPlugin.scala
+++ auron-spark-ui/src/main/scala/org/apache/spark/sql/execution/ui/AuronSQLHistoryServerPlugin.scala
@@ -31,7 +31,7 @@ class AuronSQLHistoryServerPlugin extends AppHistoryServerPlugin {
 
   override def setupUI(ui: SparkUI): Unit = {
     val sqlStatusStore = new AuronSQLAppStatusStore(ui.store.store)
-    if (sqlStatusStore.buildInfo() != null) {
+    if (sqlStatusStore.executionsCount() > 0) {
       new AuronSQLTab(sqlStatusStore, ui)
     }
   }
