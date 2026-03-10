8949df6cf18c45fcae5a888c99d45e62f1225aaf
guihuawen <guihuawen@didiglobal.com>
2026-03-10 15:41:04 +0800

    test_hive_paimon

---
+++ thirdparty/auron-paimon/src/test/scala/org/apache/auron/AuronPaimonSuite.scala
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.auron
+
+import org.apache.spark.sql.AuronQueryTest
+
+class AuronPaimonSuite extends AuronQueryTest with BaseAuronPaimonSQLSuite {
+
+  test("test paimon with partition table error") {
+    withTable("t1") {
+      sql("create table t1 (id string) partitioned by(pt string) TBLPROPERTIES ('file.format' = 'parquet')  ")
+      sql("alter ")
+      sql("insert into t1 partition(pt = '2026-03-10') values('1')")
+      val df = sql("select * from t1 where pt = '2026-03-10'")
+      df.show()
+      assert(df.collect().toList.head.get(0) == "1")
+      assert(df.collect().toList.head.get(1) == "2026-03-10")
+    }
+  }
+
+}
---
+++ thirdparty/auron-paimon/src/test/scala/org/apache/auron/BaseAuronSQLSuite.scala
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.auron
+
+import java.io.File
+
+import org.apache.commons.io.FileUtils
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.test.SharedSparkSession
+
+import org.apache.auron.util.SparkVersionUtil
+
+trait BaseAuronPaimonSQLSuite extends SharedSparkSession {
+  protected val suiteWorkspace: String = getClass.getResource("/").getPath + "auron-tests-workdir"
+  protected val warehouseDir: String = suiteWorkspace + "/spark-warehouse"
+  protected val metastoreDir: String = suiteWorkspace + "/meta"
+
+  protected def resetSuiteWorkspace(): Unit = {
+    val workdir = new File(suiteWorkspace)
+    if (workdir.exists()) {
+      FileUtils.forceDelete(workdir)
+    }
+    FileUtils.forceMkdir(workdir)
+    FileUtils.forceMkdir(new File(warehouseDir))
+    FileUtils.forceMkdir(new File(metastoreDir))
+  }
+
+  override def beforeAll(): Unit = {
+    // Prepare a clean workspace before SparkSession initialization
+    resetSuiteWorkspace()
+    super.beforeAll()
+    spark.sparkContext.setLogLevel("WARN")
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+  }
+
+  override protected def sparkConf: SparkConf = {
+    val conf = super.sparkConf
+      .set(
+        "spark.sql.extensions",
+        "org.apache.spark.sql.auron.AuronSparkSessionExtension," +
+          "org.apache.paimon.spark.extensions.PaimonSparkSessionExtensions")
+      .set("spark.memory.offHeap.enabled", "false")
+      .set("spark.auron.enable", "true")
+      .set("spark.ui.enabled", "false")
+      .set("spark.sql.warehouse.dir", warehouseDir)
+      .set("spark.sql.catalog.paimon.warehouse", warehouseDir)
+      .set("spark.auron.udf.singleChildFallback.enabled", "false")
+      .set("spark.sql.catalog.paimon", "org.apache.paimon.spark.SparkCatalog")
+      .set(
+        "spark.shuffle.manager",
+        "org.apache.spark.sql.execution.auron.shuffle.AuronShuffleManager")
+    if (SparkVersionUtil.isSparkV40OrGreater) {
+      // Spark 4.0+: Disable session artifact isolation, align with Spark 3.x behavior
+      // Fix Catalyst codegen failure: prevent org.apache.spark.sql.catalyst.expressions.Object
+      // in isolated dirs from REPL classloader lookup failure
+      conf.set("spark.sql.artifact.isolation.enabled", "false")
+    }
+    conf
+  }
+}
